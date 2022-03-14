fa224a1ea731ba5ff2c831c3783753d0dbda2a5d
michael03nyamwaya <michaelnyamwaya03@gmail.com>
2022-03-14 12:20:02 -0700

    question

--- alx-low_level_programming/0x01-variables_if_else_while/1-last_digit.c
+++ alx-low_level_programming/0x01-variables_if_else_while/1-last_digit.c
@@ -1,15 +1,47 @@
-if (last > 5)
+nclude <stdio.h>
 
-			printf("Last digit of %d is %d and is greater than 5\n", n, last);
+#include <stdlib.h>
 
-				else if (last == 0)
+#include <time.h>
 
-			printf("Last digit pf %d is %d and is 0\n", n, last);
+/**
+ *
+ *  * main - print last digit in random number
+ *
+ *   *
+ *
+ *    * Description: print last digit in random number
+ *
+ *     *
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *       */
 
-					printf("Last digit of %d is %d and is 0\n", n, last);
+int main(void)
 
-						else if (last < 6 && last != 0)
+{
 
-			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+		int n, last;
 
-				return (0);
+			srand(time(0));
+
+				n = rand() - RAND_MAX / 2;
+
+					last = n % 10;
+
+						if (last > 5)
+
+									printf("Last digit of %d is %d and is greater than 5\n", n, last);
+
+							else if (last == 0)
+
+										printf("Last digit pf %d is %d and is 0\n", n, last);
+
+									printf("Last digit of %d is %d and is 0\n", n, last);
+
+										else if (last < 6 && last != 0)
+
+													printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+
+											return (0);
