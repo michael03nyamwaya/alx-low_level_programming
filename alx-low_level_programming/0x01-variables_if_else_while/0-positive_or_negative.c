10b6b842b08bc25b751fc1bac79ff7d646772b1c
michael03nyamwaya <michaelnyamwaya03@gmail.com>
2022-03-14 07:50:37 -0700

    My commit message

---
+++ alx-low_level_programming/0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main -Entry point
+*
+* Return: Always 0 (Success/correct)
+*/
+
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAx / 2;
+
+if (n==0)
+{
+printf("%i is zero\n", n);
+}
+else if (n<O)
+{
+printf("%i is negative\n", n);
+}
+else
+{
+printf("%i is postive\n", n);
+}
+return (0);
+}
