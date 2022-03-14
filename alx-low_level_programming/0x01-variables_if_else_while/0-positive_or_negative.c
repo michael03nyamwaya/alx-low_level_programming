fe4f7c0bf2f1ac77ed7c380c00137b38894b46b7
michael03nyamwaya <michaelnyamwaya03@gmail.com>
2022-03-14 09:36:45 -0700

    commit messages

--- alx-low_level_programming/0x01-variables_if_else_while/0-positive_or_negative.c
+++ alx-low_level_programming/0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,31 +1,58 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
+#include <stdlib.h>
+
+#include <time.h>
+
+
+
 /**
-* main -Entry point
-*
-* Return: Always 0 (Success/correct)
-*/
+ *
+ *  * main - determine if number is positive or negative
+ *
+ *   *
+ *
+ *    * Description: program will assign a random number to the variable n each time
+ *
+ *     * it is executed.
+ *
+ *      *
+ *
+ *       * Return: Always 0 (Success)
+ *
+ *        */
+
+
 
 int main(void)
-{
-int n;
 
-srand(time(0));
-n = rand() - RAND_MAx / 2;
+{
 
-if (n==0)
-{
-printf("%i is zero\n", n);
+		int n;
+
+
+
+
+
+			srand(time(0));
+
+				n = rand() - RAND_MAX / 2;
+
+					if (n == 0)
+
+								printf("%d is zero\n", n);
+
+						else if (n < 0)
+
+									printf("%d is negative\n", n);
+
+							else
+
+										printf("%d is positive\n", n);
+
+
+
+								return (0);
+
 }
-else if (n<O)
-{
-printf("%i is negative\n", n);
-}
-else
-{
-printf("%i is postive\n", n);
-}
-return (0);
-}
+
