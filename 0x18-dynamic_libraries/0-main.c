a50754df2f4e0d8fce10f86de5bb1cead914b41e
root <michaelnyamwaya03@gmail.com>
2022-06-17 17:08:37 -0700

    libdynamics

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -2,30 +2,12 @@
 #include <stdio.h>
 
 /**
-<<<<<<< HEAD
- * _isupper - a function that checks for uppercase characters
- * @c: a variable that stores the letters
- * Return: Return 1 if c is uppercase and 0 otherwise
- */
-
-int _isupper(int c)
-{
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-=======
  * main - check the code
  *
- * Return: Always EXIT_SUCCESS.
+ *  Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
-    printf("%d\n", _strlen("My Dyn Lib"));
-    return (EXIT_SUCCESS);
->>>>>>> be5c301a918c2a5b2317c356e2fddea39675fe89
+	printf("%d\n", _strlen("My Dyn Lib"));
+	return (EXIT_SUCCESS);
 }
